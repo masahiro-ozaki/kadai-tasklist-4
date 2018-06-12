d4c29ca06acaf9569ad33af4de59c73f357511e3
masahiro.ozaki <masahiro.b.ozaki@rakuten.com>
2018-06-12 11:51:20 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@
 |
 */
 
-/*
+
 Route::get('/', function () {
     return view('welcome');
 });
-*/
 
-Route::get('/', 'TasksController@index');
 
-Route::resource('tasks', 'TasksController');
+//Route::get('/', 'TasksController@index');
+
+//Route::resource('tasks', 'TasksController');
