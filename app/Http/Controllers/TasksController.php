5642d72d294867be17b8dc169fdb9a36637c27b8
masahiro.ozaki <masahiro.b.ozaki@rakuten.com>
2018-06-15 00:50:30 +0000

    add auth

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -135,17 +135,24 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $this->validate($request, [
-            'status' => 'required|max:10',
-            'content' => 'required|max:191',
-        ]);
-        
         $task = Task::find($id);
-        $task->status = $request->status; 
-        $task->content = $request->content;
-        $task->save();
-
-        return redirect('/');
+        
+        if (\Auth::user()->id === $task->user_id){
+        
+            $this->validate($request, [
+                'status' => 'required|max:10',
+                'content' => 'required|max:191',
+            ]);
+            
+            $task = Task::find($id);
+            $task->status = $request->status; 
+            $task->content = $request->content;
+            $task->save();
+    
+            return redirect('/');
+        } else {
+            return redirect('/');
+        }
     }
 
     /**
